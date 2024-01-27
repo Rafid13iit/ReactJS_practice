22fb5427f9d6d29cbf0bba61e60b2eeec5a1ada2
Rafid13iit <rafidssrr.6767@gmail.com>
2024-01-27 11:54:17 +0600

    new files created

---
+++ Practise01/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello and welcome to practise projects!</h1>
+      
+    </>
+  )
+}
+
+export default App
---
+++ Practise01/src/components/Header.jsx
@@ -0,0 +1,21 @@
+
+const header = () => {
+    return (
+        <div>
+            <h2>Header Section!</h2>
+        </div>
+    );
+};
+
+export default header;
+
+
+
+
+// const header = () => {
+//   return (
+//     <div>header</div>
+//   )
+// }
+
+// export default header
